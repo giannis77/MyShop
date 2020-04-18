1df1dae98af7e7b8c01b700b859fa7e6a5b5c76d
Giannis Avraam <giannis_avr@hotmail.com>
2020-04-18 23:48:28 +0300

    Product Model ans InMemory Product Repository Built

---
+++ MyShop/MyShop.Core/Models/Product.vb
@@ -0,0 +1,23 @@
+﻿Imports System.ComponentModel
+Imports System.ComponentModel.DataAnnotations
+
+Public Class Product
+
+    Public Sub New()
+        Me.Id = Guid.NewGuid.ToString()
+    End Sub
+
+    Public Property Id As String
+
+    <StringLength(20)>
+    <DisplayName("Product Name")>
+    Public Property Name As String
+
+    <Range(0, 1000)>
+    Public Property Price As Double
+
+    Public Property Description As String
+    Public Property Category As String
+    Public Property Image As String
+
+End Class
---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Runtime.Caching
+Imports MyShop.Core
+
+Public Class ProductRepository
+    Public Cache As ObjectCache = MemoryCache.Default
+
+    Public lstProducts As List(Of Product)
+
+    Public Sub New()
+
+        lstProducts = Cache("lstProduct")
+
+        If lstProducts Is Nothing Then
+            lstProducts = New List(Of Product)
+        End If
+    End Sub
+
+    Public Sub Commit()
+
+        Cache("lstProducts") = lstProducts
+
+    End Sub
+    Public Sub Insert(ByVal oProduct As Product)
+        lstProducts.Add(oProduct)
+    End Sub
+
+    Public Sub Update(ByVal oProduct As Product)
+        Dim oProductToUpdate As Product
+
+        oProductToUpdate = New Product
+
+        oProductToUpdate = lstProducts.Find(Function(p) p.Id = oProduct.Id)
+
+        If Not oProductToUpdate Is Nothing Then
+            oProductToUpdate = oProduct
+        Else
+            Throw New Exception("Product Not Found")
+        End If
+    End Sub
+
+    Public Function Find(ByVal sId As String)
+        Dim oProductFind As Product
+
+        oProductFind = New Product
+
+        oProductFind = lstProducts.Find(Function(p) p.Id = sId)
+
+        If Not oProductFind Is Nothing Then
+            Return oProductFind
+        Else
+            Throw New Exception("Product Not Found")
+        End If
+    End Function
+
+    Public Function Collection() As IQueryable(Of Product)
+        Return lstProducts.AsQueryable()
+    End Function
+
+
+    Public Sub Delete(ByVal oProduct As Product)
+        Dim oProductToDelete As Product
+
+        oProductToDelete = New Product
+
+        oProductToDelete = lstProducts.Find(Function(p) p.Id = oProduct.Id)
+
+        If Not oProductToDelete Is Nothing Then
+            lstProducts.Remove(oProductToDelete)
+        Else
+            Throw New Exception("Product Not Found")
+        End If
+    End Sub
+End Class
