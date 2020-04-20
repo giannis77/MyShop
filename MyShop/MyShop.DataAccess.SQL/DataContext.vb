d76bf948db36870320661896b448c69a0d389090
Giannis Avraam <giannis_avr@hotmail.com>
2020-04-20 15:36:22 +0300

    EntityFramework- SoS

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Data.Entity
+Imports MyShop.Core
+
+Public Class DataContext
+    Inherits DbContext
+
+    Public Sub New()
+        MyBase.New("DefaultConnection")
+    End Sub
+
+    Public Property Products As DbSet(Of Product)
+    Public Property ProductCategories As DbSet(Of ProductCategory)
+End Class
---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202004201229378_Initial.vb
@@ -0,0 +1,41 @@
+Imports System
+Imports System.Data.Entity.Migrations
+Imports Microsoft.VisualBasic
+
+Namespace Migrations
+    Public Partial Class Initial
+        Inherits DbMigration
+    
+        Public Overrides Sub Up()
+            CreateTable(
+                "dbo.ProductCategories",
+                Function(c) New With
+                    {
+                        .Id = c.String(nullable := False, maxLength := 128),
+                        .Category = c.String(),
+                        .CreatedAt = c.DateTimeOffset(nullable := False, precision := 7)
+                    }) _
+                .PrimaryKey(Function(t) t.Id)
+            
+            CreateTable(
+                "dbo.Products",
+                Function(c) New With
+                    {
+                        .Id = c.String(nullable := False, maxLength := 128),
+                        .Name = c.String(maxLength := 20),
+                        .Price = c.Decimal(nullable := False, precision := 18, scale := 2),
+                        .Description = c.String(),
+                        .Category = c.String(),
+                        .Image = c.String(),
+                        .CreatedAt = c.DateTimeOffset(nullable := False, precision := 7)
+                    }) _
+                .PrimaryKey(Function(t) t.Id)
+            
+        End Sub
+        
+        Public Overrides Sub Down()
+            DropTable("dbo.Products")
+            DropTable("dbo.ProductCategories")
+        End Sub
+    End Class
+End Namespace
